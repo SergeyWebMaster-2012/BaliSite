03784f731fb9092040cfc59bb520fd2eb9015304
SergeyWebMaster <onboxs@yandex.ru>
2021-02-15 15:03:48 +0400

    закончил верстку

--- js/main.js
+++ js/main.js
@@ -12,3 +12,24 @@ $(".program-slider").slick({
     dots: true,
     dotsClass:"programs-dots"
 })
+
+$(".photo-slider--up").slick({
+    arrows: false,
+    dots: false,
+    sladesToShow:3,
+    sladesToScroll:1,
+    variableWidth: true,
+    infinite: true,
+      speed: 300
+})
+
+$(".photo-slider--down").slick({
+    arrows: false,
+    dots: false,
+    sladesToShow:3,
+    sladesToScroll:1,
+    variableWidth: true,
+    infinite: true,
+      speed: 300
+
+})
